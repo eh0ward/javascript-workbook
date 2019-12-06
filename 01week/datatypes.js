322fc51f24175fda36021c8633fcd347195b27a0
eh0ward <surpmusic@gmail.com>
2019-12-05 18:54:15 -0600

    homework

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,1 +1,18 @@
-console.log("hi");
+//Write a Javascript program display the current date and time.
+
+let currentDate = new Date();
+console.log("The current date and time is", currentDate);
+
+//Write a JavaScript program to convert a number to a string.
+var num = 15;
+var n = num.toString();
+console.log("The number is", num);
+console.log("The string representing the number is", n);
+
+//Write a JavaScript program to convert a string to the number.
+var name = "John Doe";
+var age = 12;
+var isMale = true;
+
+console.log("The name variable is a", typeof name);
+console.log("The age variable is a", typeof age);
