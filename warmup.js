442c7a924089b760f9a81bfde24ed6c5e84ce0c5
eh0ward <surpmusic@gmail.com>
2019-12-17 19:38:44 -0600

    warmup 03week Tuesday

---
+++ warmup.js
@@ -0,0 +1,42 @@
+import { setupMaster } from "cluster";
+
+// 03week Tuesday warmUp
+// 1. Given a number List(array) of numbers write an algorithm that finds the smallest number.Given
+
+// 2. Implement your algorithm as a function.
+
+// function min(list) {
+//     .
+//     .
+//     .
+//     .
+//     .
+
+//     return results
+// }
+
+let someHugeList = [13,56, 86, 89, 4, 50 ,67, 12, 16, 17]
+
+function min(list){
+    let mySmallestSoFar = list[0];
+    for(let i = 0 ; i < list.lenght, i++){
+        let fromThePile = list[i]
+    }
+}
+
+
+// return the sum of all numbers in the list
+function sum(List){
+    let sum = 0;
+    for(let j=0; j<list.lenght; j++) {
+        result = sum + list[j];
+    }
+
+    return sum;
+}
+
+let emptyList = [];
+console.log("the sum of the number in the list is", sum(someHugeList));
+
+var name = 'John'
+console.log(name.toUpperCase());
