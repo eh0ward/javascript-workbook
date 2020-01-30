538c94b51efa64f7ccf7aa03a117d4488933499d
eh0ward <surpmusic@gmail.com>
2020-01-30 17:25:45 -0600

    addressBook start

---
+++ addressBook/address.js
@@ -0,0 +1,48 @@
+console.log("In address.js");
+
+window.onload = function() {
+  console.log("inside window.onload");
+  fetchUsers();
+};
+
+function fetchUsers() {
+  console.log("About to call api");
+  fetch("https://randomuser.me/api/?results=10")
+    .then(function(response) {
+      console.log("processing the response", response);
+      // do something with the response once I get it
+      return response.json();
+    })
+    .then(function(myJson) {
+      //   do something with the json payload
+      console.log("process json payload", myJson);
+      processUsers(myJson.results);
+    });
+}
+
+function processUsers(listOfUsers) {
+  listOfUsers.forEach(function(user) {
+    console.log("Process a single user", user);
+    // let myUl = document.getElementById("contacts");
+    // let anLi = document.createElement("li");
+  });
+}
+
+// let arrayOfUsers;
+
+// const newUser = () => {
+//   fetch("https://randomuser.me/api/")
+//   .then(function(result) {
+//       return result.json()
+//   } .then(function(randomUser) {
+//       arrayOfUsers =
+//       console.log(randomUser);
+//       display(randomUser)
+//   }
+//     // .then(res => res.json())
+//     // .then(user => (arrayOfUsers = user));
+// };
+
+// const consoleUsers = () => {
+//   console.log(arrayOfUsers);
+// };
