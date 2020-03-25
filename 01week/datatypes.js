06154d2e1b0ddf1ae0d517199272013359df080b
eh0ward <surpmusic@gmail.com>
2020-03-25 06:06:00 -0500

    dataTypes start

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,82 +1,172 @@
-//Write a Javascript program display the current date and time.
+"use strict";
 
-let currentDate = new Date();
-console.log("The current date and time is", currentDate);
+console.log("hello");
+
+//Write a Javascript program display the current date and time.
+console.log("");
+console.log("-------------------------------");
+console.log("The Date and Time");
+console.log("");
+
+let myDate = new Date();
+
+console.log("the current date and time is", myDate);
 
 //Write a JavaScript program to convert a number to a string.
-var num = 15;
-var n = num.toString();
-console.log("The number is", num);
-console.log("The string representing the number is", n);
+console.log("");
+console.log("-------------------------------");
+console.log("Converts A Number To A String:");
+console.log("");
+
+let age = 40;
+let thisMany = age.toString();
+
+console.log("Age:", age);
+console.log("Eric is", thisMany, "years old");
 
 //Write a JavaScript program to convert a string to the number.
-var name = "John Doe";
-var age = 12;
-var isMale = true;
+console.log("");
+console.log("-------------------------------");
+console.log("Converts A String To The Number:");
+console.log("");
 
-console.log("The name variable is a", typeof name);
-console.log("The age variable is a", typeof age);
+let name = "Eric Howard";
+let eH = name.toString();
+let string = age;
+
+console.log(eH, "age is", string);
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
 // Boolean
-console.log(17 === 17);
-// Null
-let d = null;
+console.log("");
+console.log("-------------------------------");
+console.log("Writing A Boolean:");
+console.log("");
 
-console.log(d);
+console.log(17 === 17);
+
+// Null
+console.log("");
+console.log("-------------------------------");
+console.log("Null:");
+console.log("");
+
+let a = null;
+
+console.log(a);
 
 // Undefined
+console.log("");
+console.log("-------------------------------");
+console.log("Undefined:");
+console.log("");
+
 const u = undefined;
 
 console.log(u);
 
 // Number
-const c = 8;
+console.log("");
+console.log("-------------------------------");
+console.log("A Number:");
+console.log("");
 
-console.log(c + " is a number");
+const b = 8;
+
+console.log(b, "is a number");
 
 // NaN
-const t = 5;
+console.log("");
+console.log("-------------------------------");
+console.log("NaN:");
+console.log("");
 
-console.log(u + t);
+let z = 5;
+
+console.log(u + z);
 
 // String
+console.log("");
+console.log("-------------------------------");
+console.log("A String:");
+console.log("");
+
 const s = "This is a string";
 
 console.log(s);
 
 // Write a JavaScript program that adds 2 numbers together.
-var num1 = 3;
-var num2 = 5;
+console.log("");
+console.log("-------------------------------");
+console.log("Add 2 Number Together:");
+console.log("");
 
-console.log(num1 + num2);
+let num1 = 2036;
+let num2 = 1000;
+
+console.log("2036 + 1000 =", num1 + num2);
 
 // Write a JavaScript program that runs only when 2 things are true.
-let f = 8;
-let g = 2;
+console.log("");
+console.log("-------------------------------");
+console.log("Runs Only When 2 Things Are True:");
+console.log("");
 
-if (f > g) {
-  console.log(true);
+let d = 8;
+let e = 2;
+let k = 1;
+let t = true;
+
+if (d > e && d * k === 8) {
+  console.log("Both conditions are", t);
+} else {
+  console.log("false");
 }
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
-let h = 8;
-let i = 2;
+console.log("");
+console.log("-------------------------------");
+console.log("Runs When 1 of 2 Things Are True:");
+console.log("");
 
-if (h < i) {
-  console.log(h + "is less than " + i + "= False");
-} else if (h > i) {
-  console.log(h + " is greater than " + i + " = True");
+let g = 17;
+let h = 3;
+
+if (g < h || g <= g) {
+  console.log("1 0f 2 Conditions", t);
+} else {
+  console.log("false");
 }
 
 // Write a JavaScript program that runs when both things are not true.
-let p = 9;
-let o = 10;
+console.log("");
+console.log("-------------------------------");
+console.log("Runs When Both Things Are NOT True:");
+console.log("");
 
-if (p > o) {
-  console.log(p + " is greater than " + o);
-} else if (p === o) {
-  console.log(p + " is equal to " + o);
+let i = 9;
+let j = 10;
+let f = false;
+
+if (i > j) {
+  console.log(i, "is greater than", j);
+} else if (i === j) {
+  console.log(i, "is equal to", j);
 } else {
-  console.log("Both things are not true");
+  console.log("Both conditions are", f);
 }
+
+console.log("-------------------------------");
+console.log("");
+
+// +++++ PART TWO - THE DOM +++++
+
+// get the span from the page/document
+// let mySpan = document.getElementById("theTime");
+// change what the inner text of the span says
+// mySpan.innerText = myDate.toString();
+
+// let mySpan = document.getElementById("theTime");
+// mySpan.addEventListener("click", function() {
+//   mySpan.innerText = myDate.toString();
+// });
